8d7d6eedc32ce422f8869585c80a6624a9c9de77
MashaVtyurina <149298116+MashaVtyurina@users.noreply.github.com>
2025-03-15 23:51:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from logging import LOGGER
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='CLI for sending msgs')
-    parser.add_argument('--sender', help='Sender\'s number')
-    parser.add_argument('--recipient', help='Receiver\'s number')
-    parser.add_argument('--msg', help='Message')
+    parser = argparse.ArgumentParser(description='CLI for sending messages')
+    parser.add_argument('sender', help='Sender:')
+    parser.add_argument('recipient', help='Receiver:')
+    parser.add_argument('msg', help='Message')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
-    LOGGER.info(f'Sender {args.sender} sent to recipient {args.recipient} msg {args.msg}')
+    LOGGER.info(f' {args.sender} has just sent to {args.recipient} message: {args.msg}')
     response = send_msg(args.sender, args.recipient, args.msg)
-    LOGGER.info(f'Client got a response from server: {response}')
+    LOGGER.info(f'Client received a this server-response: {response}')
     print(response)
